89b1d7d477dc8c45c0553dde98e65cf7ad3e919d
MRubaek <90452805+MRubaek@users.noreply.github.com>
2021-09-13 15:41:08 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -11,22 +11,104 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
 
-ev3=EV3Brick()
+
+# Create your objects here.
+ev3 = EV3Brick()
+
+"""
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
-
+"""
 # Initialize the color sensor.
-line_sensor = ColorSensor(Port.S3)
-claw_motor = Motor(Port.A, Direction.COUNTERCLOCKWISE, [8,400])
+#line_sensor = ColorSensor(Port.S3)
+
+claw_motor = Motor(Port.A, Direction.COUNTERCLOCKWISE, [8,700])
+
+
+"""
+
+robot=DriveBase(left_motor, right_motor, wheel_diameter=11, axle_track=104)
+
+robot.straight(500)
+
+"""
+#claw_motor.run_time(-30, 500)
+#claw_motor.run(30)
+
+
+"""claw_motor.run_time(-30,2500)
+claw_motor.run(30)
+"""
+sekvens=1
+
+if sekvens == 1:
+    claw_motor.run_time(30, 2500)
+    claw_motor.run(30)
+    sekvens+=1
+
+if sekvens ==2:
+    claw_motor.run_time(-30, 2500)
+    claw_motor.run(-30)
+    sekvens+=1
+
+if sekvens ==3:
+    claw_motor.run_time(15, 2500)
+    claw_motor.run(30)
+    sekvens +=1
+
+if sekvens == 4:
+    claw_motor.run_time(-30,3000)
+    claw_motor.run(30)
+
+"""
+wait(1500)
+
+claw_motor.run_time(30, 3000)
+claw_motor.run(30)
+
+wait(1500)
+
+claw_motor.run_time(30, 10000)
+claw_motor.run(30)
+"""
+"""
+sound_sensor = UltrasonicSensor(Port.S2)
 
 # Initialize the drive base.
-robot = DriveBase(left_motor, right_motor, wheel_diameter=34, axle_track=14)
+#robot = DriveBase(left_motor, right_motor, wheel_diameter=34, axle_track=14)
+while True:
+    #if sound_sensor.distance() < 250:
+    #    ev3.speaker.beep()
+    print(sound_sensor.distance())
+    #wait(1000)
 
+
+    if sound_sensor.distance() < 300:
+        ev3.speaker.beep()
+        wait(1000)
+         # Turn around by 45 degrees
+        print("robot.turn(45)")
+        wait(1000)
+        print("robot.drive(100, 0)")
+        wait(1000)
+        print("robot.turn(-45)")
+        wait(1000)
+        print("robot.drive(50, 0)")
+        wait(1000)
+        print("robot.turn(-45)")
+        wait(1000)
+        print("robot.drive(100, 0)")
+        wait(1000)
+        print("robot.turn(45)")
+        wait(1000)
+        print("robot.drive(100, 0)")
+"""
 # Calculate the light threshold. Choose values based on your measurements.
+"""
 BLACK = 9
 WHITE = 85
 threshold = (BLACK + WHITE) / 2
-
+"""
 # Set the gain of the proportional line controller. This means that for every
 # percentage point of light deviating from the threshold, we set the turn
 # rate of the drivebase to 1.2 degrees per second.
@@ -36,9 +118,10 @@ grey: -5 10
 white 30 55
 black -45 -35
 '''
-claw_motor.run_time(30, 10000)
-claw_motor.run(10000)
-'''
+#claw_motor.run_time(30, 10000)
+#claw_motor.run(10000)
+
+"""
 def straightenUp (section, deviation):
     if section == 0 and 1 and 3:
         while deviation >= 30:
@@ -49,8 +132,9 @@ def straightenUp (section, deviation):
             robot.turn(-45)
             deviation = line_sensor.reflection() - threshold
         
-       
+"""
 
+"""      
 section = 0
 # Start following the line endlessly.
 while section == 0:
@@ -58,12 +142,10 @@ while section == 0:
     deviation = line_sensor.reflection() - threshold
     
     robot.drive(100,0)
-
     if deviation >=30: #White
         #straighten up
         straightenUp(section, deviation)
         #robot.turn(45)
-
         
     if deviation <= -35: #Black
         #new section
@@ -73,7 +155,5 @@ while section == 0:
     print(deviation)
     # You can wait for a short time or do other things in this loop.
     wait(10)
-
 while section == 1:
-
-'''
+"""
